88e0ecae700506984482542d70bbae093cb876b2
Aarshay Jain <aarshay.jain@columbia.edu>
2023-04-28 14:40:23 +0530

    bump version dev

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # code-genie
 This library is your copilot for jupyter notebooks
 
-Latest version: 0.1.2
+Latest version: 0.2.0-dev0
 
 ## Documentation
 
--- code_genie/__init__.py
+++ code_genie/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.1.2"
+__version__ = "0.2.0-dev0"
 
 from code_genie.genie import Genie, PandasGenie
 from code_genie._config import set_options
